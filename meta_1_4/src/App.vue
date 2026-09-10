d8a409163222b671972d8b0a9164c9b984f90164
AmericaMasse <america.masse@uabc.edu.mx>
2026-09-10 12:34:46 -0700

    Cambio de estructura, creacion de archivos .vue e implementacion del componente TarjetaConImagen.vue

--- meta_1_4/src/App.vue
+++ meta_1_4/src/App.vue
@@ -1,11 +1,16 @@
+<script setup>
+import TarjetaConImagen from './components/TarjetaConImagen.vue'
+</script>
+
 <template>
   <v-app>
     <v-main>
-      <router-view />
+      <TarjetaConImagen
+        image-url="https://picsum.photos/id/10/300/200"
+        title="Tarjeta de Prueba"
+        description="Probando el componente"
+        author="Autor de prueba"
+      />
     </v-main>
   </v-app>
 </template>
-
-<script lang="ts" setup>
-  //
-</script>
---
+++ meta_1_4/src/components/TarjetaConImagen.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-card class="mx-auto" max-width="400">
+    <v-img :src="imageUrl" height="200" cover></v-img>
+    <v-card-title>{{ title }}</v-card-title>
+    <v-card-subtitle>Foto por: {{ author }}</v-card-subtitle>
+    <v-card-text>{{ description }}</v-card-text>
+  </v-card>
+</template>
+
+<script setup>
+defineProps({
+  imageUrl: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    default: 'Sin título'
+  },
+  description: {
+    type: String,
+    default: ''
+  },
+  author: {
+    type: String,
+    default: 'Autor desconocido'
+  }
+})
+</script>
